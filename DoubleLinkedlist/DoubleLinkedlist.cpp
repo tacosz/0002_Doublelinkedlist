dcf2d59d1376b1e0fb303f1ed315d966ab460b7e
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-04 11:27:12 +0700

    penambahan if dalam else

--- DoubleLinkedlist/DoubleLinkedlist.cpp
+++ DoubleLinkedlist/DoubleLinkedlist.cpp
@@ -38,7 +38,7 @@ void addNode()
     newNode->prev = NULL;   //step 5: make the new node point to NULL
     START = newNode;        //step 6: make the new node the first node
 }
-else
+ else
 {
     // insert the new node in the middle or at the end
     Node *current = START; // step 1.a: start from the first node
@@ -48,6 +48,14 @@ else
     {                               // step 1.c: traverse the list to find the correct position
         previous = current;         // step 1.d: move the previous to the current node
         current = current->next;    // step 1.e: move the current to the next node
+    }
+
+    newNode->next = current; // step 4: Make the next field of the current node point to the new nod
+    newNode->prev = previous; // step 5: Make the previous field of the new node point to previous
+
+    if (current != NULL)
+    {
+        current->prev = newNode; // step 6: Make the previous field of the current node point to the new node
     }
 }
 
