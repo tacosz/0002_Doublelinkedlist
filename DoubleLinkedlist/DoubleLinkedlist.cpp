8a0cd262a94101a00c989f1ed7d327a3c4aef5a4
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-04 11:42:54 +0700

    penambahan if dalam fungsi void

--- DoubleLinkedlist/DoubleLinkedlist.cpp
+++ DoubleLinkedlist/DoubleLinkedlist.cpp
@@ -82,6 +82,24 @@ bool search(int rollNo, Node** previous, Node** current)
     return (*current != NULL);
 }
 
+void deleteNode()
+{
+    Node* previous, * current;
+    int rollNo;
+
+    cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+    cin >> rollNo; // step 3: get the roll number to be deleted
+
+    if (START == NULL)
+    {
+        cout << "List is empty" << endl;
+        return;
+    }
+
+    current = START; // step 1: start from the first node
+    previous = NULL;
+}
+
 
 int main()
 {
