ec90bf4e7a9139b8afba1371f42f9c0bb665fde4
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-04 10:59:46 +0700

    penambahan if di dalam if

--- DoubleLinkedlist/DoubleLinkedlist.cpp
+++ DoubleLinkedlist/DoubleLinkedlist.cpp
@@ -18,6 +18,17 @@ void addNode()
     cin >> newNode->noMhs; // Assign value to the data field of the new node
     cout << "\nEnter the name of the student: ";
     cin >> newNode->name; // Assign value to the data field of the new node
+
+    //insert the new node in the list
+    if (START == NULL || newNode->noMhs <= START->noMhs)
+    { // step 2: insert the new node at the beginning
+
+        if (START != NULL && newNode->noMhs == START->noMhs)
+        {
+            cout << "\033[31Dumplicate roll numbers not allowed\033[0m" << endl;
+            return;
+        }
+    }
 }
 
 int main()
