3dcfd3c317bc07109a13f982f2ca7a3a3fbded12
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-04 11:29:25 +0700

    penambahan if dalam else

--- DoubleLinkedlist/DoubleLinkedlist.cpp
+++ DoubleLinkedlist/DoubleLinkedlist.cpp
@@ -57,6 +57,12 @@ void addNode()
     {
         current->prev = newNode; // step 6: Make the previous field of the current node point to the new node
     }
+
+    if (previous != NULL)
+    {
+        previous->next = newNode; // step 7: Make the next field of the previous node point to the new node
+    }
+
 }
 
 int main()
