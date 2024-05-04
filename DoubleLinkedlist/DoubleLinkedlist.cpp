3f008633e81d89691fb735c6f9154485cc5b06da
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-04 10:44:46 +0700

    Add project files.

---
+++ DoubleLinkedlist/DoubleLinkedlist.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int noMhs;
+    string name;
+    Node* next;
+    Node* prev;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
