1934c777c00ea09875f9b2c5e8133ecc1d44741d
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-04 19:04:03 +0700

    fungsi void search data

--- DoubleLinkedlist/DoubleLinkedlist.cpp
+++ DoubleLinkedlist/DoubleLinkedlist.cpp
@@ -174,6 +174,26 @@ void revtraverse()
     }
 }
 
+void searchData()
+{
+    if (listEmpty() == true)
+    {
+        cout << "\nList is emppty" << endl;
+    }
+    Node* prev, * curr;
+    prev = curr = NULL;
+    cout << "\nEnter the roll number of the student whose record you want to search: ";
+    int num;
+    cin >> num;
+    if (search(num, &prev, &curr) == false)
+        cout << "\nRecord not found" << endl;
+    else
+    {
+        cout << "\nRecord found" << endl;
+        cout << "\nRoll number: " << curr->noMhs << endl;
+        cout << "\nName: " << curr->name << endl;
+    }
+}
 
 int main()
 {
