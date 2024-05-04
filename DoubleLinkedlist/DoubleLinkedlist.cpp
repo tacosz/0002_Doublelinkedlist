262cc588de4502bb9cc6bbef2d1a0f59eaeee8e9
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-04 19:14:27 +0700

    fungsi program utama

--- DoubleLinkedlist/DoubleLinkedlist.cpp
+++ DoubleLinkedlist/DoubleLinkedlist.cpp
@@ -211,7 +211,35 @@ int main()
             cout << "\nEnter your choice (1-6): ";
             char ch;
             cin >> ch;
+
+            switch (ch)
+            {
+            case '1':
+                addNode();
+                break;
+            case '2':
+                deleteNode();
+                break;
+            case '3':
+                traverse();
+                break;
+            case '4':
+                revtraverse();
+                break;
+            case '5':
+                searchData();
+                break;
+            case '6':
+                return 0;
+            default:
+                cout << "\nInvalid option" << endl;
+                break;
+
+            }
+        }
+        catch (exception& e)
+        {
+            cout << "Check for the values entered." << endl;
         }
     }
-    std::cout << "Hello World!\n";
 }
